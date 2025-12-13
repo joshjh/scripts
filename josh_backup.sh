53e2f4f044c189cdc6f9862fc283893903881094
Josh <josh@joshjh.dev>
2025-12-13 14:36:30 +0000

    add notify-send

--- josh_backup.sh
+++ josh_backup.sh
@@ -8,9 +8,12 @@ fi
 
 # Execute backup commands
 echo "Starting scheduled josh backup..."
+notify-send "Starting scheduled local backup"
 
 # duplicity commands
 duplicity --no-encryption --full-if-older-than 1M /home/josh/ file:///magnetic/josh_backup_dup
 
 echo "Backup completed successfully"
+notify-send "Scheduled local backup completed successfully"
+
 exit 0
